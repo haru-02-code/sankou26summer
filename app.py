f7779b63abcfbf1ed46aacd34d69d4296f64d2fa
haru-02-code <gakkouyou0225@gmail.com>
2026-08-15 16:00:06 +0900

    必須要件(登録・一覧表示)完成

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect
 
 # mysqlライブラリの読み込み
 import mysql.connector
@@ -17,13 +17,44 @@ def conn_db():
     return conn
 
 @app.route('/')
-def hello():
-    try:
-        conn = conn_db()
-        conn.close()
-        return 'DB接続成功!'
-    except Exception as e:
-        return f'DB接続エラー: {e}'
+def index():
+    con = conn_db()
+    cur = con.cursor()
+
+    sql = "select * from movies"
+    cur.execute(sql)
+    movies = cur.fetchall()
+
+    cur.close()
+    con.close()
+
+    return render_template("index.html", movies=movies)
+
+@app.route('/add.html') 
+def add_form():
+    return render_template("add.html")
+
+@app.route('/add',methods=['POST'])
+def add_movie():
+    title = request.form['movie_name']
+    genre = request.form['movie_genre']
+    watched_date = request.form['movie_watched_date']
+    rating = request.form['movie_rating']
+    review = request.form['movie_review']
+
+    con = conn_db()
+    cur = con.cursor()
+
+    sql = "insert into movies (title, genre, watched_date, rating, review) values (%s, %s, %s, %s, %s)"
+    #SQLの実行
+    cur.execute(sql,[title, genre, watched_date, rating, review])
+    #データ確定
+    con.commit()
+
+    cur.close()
+    con.close()
+
+    return '登録できました！'
 
 if __name__ == '__main__':
     app.run(debug=True)
